34adedb787c4a71ac50292cf77eb14ddb1d0fc8c
Victor Simas <victorsimasdev@gmail.com>
2024-10-08 14:17:23 -0300

    fix: Removed unnecessary print, translated charts

--- spaceship_titanic.py
+++ spaceship_titanic.py
@@ -35,17 +35,17 @@ metrics = ['precision', 'recall', 'f1-score']
 classes = list(report.keys())[:-3]
 report_df = pd.DataFrame({
     'Classe': classes,
-    'Precisão': [report[classe]['precision'] * 100 for classe in classes],
+    'Precision': [report[classe]['precision'] * 100 for classe in classes],
     'Recall': [report[classe]['recall'] * 100 for classe in classes],
     'F1-Score': [report[classe]['f1-score'] * 100 for classe in classes]
 })
 
 ax = report_df.set_index('Classe').plot(kind='bar', figsize=(10, 6), color=['#1f77b4', '#ff7f0e', '#2ca02c'])
-plt.title('Classification Report (em Percentual)')
+plt.title('Classification Report (%)')
 plt.xlabel('Classes')
-plt.ylabel('Métricas (%)')
+plt.ylabel('Metrics (%)')
 plt.xticks(rotation=45)
-plt.legend(title='Métricas')
+plt.legend(title='Metrics')
 plt.ylim(0, 100)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 
@@ -55,22 +55,21 @@ plt.show()
 
 plt.figure(figsize=(10, 6))
 sns.histplot(data=train, x='Age', hue='Transported', bins=30, kde=True, stat='density', common_norm=False)
-plt.title('Distribuição da Idade por Status de Transporte')
-plt.xlabel('Idade')
-plt.ylabel('Densidade')
-plt.legend(title='Transportado', labels=['Não', 'Sim'])
+plt.title('Age distribution per transported status')
+plt.xlabel('Age')
+plt.ylabel('Density')
+plt.legend(title='Transported', labels=['No', 'Yes'])
 plt.show()
 
 cm = confusion_matrix(y_val, y_pred)
-print(cm)
 cm_reordered = [[cm[1, 1], cm[1, 0]],
                 [cm[0, 1], cm[0, 0]]]
 plt.figure(figsize=(8, 6))
 sns.heatmap(cm_reordered, annot=True, fmt='d', cmap='Blues', 
-            yticklabels=['Verdadeiros', 'Falsos'], 
-            xticklabels=['Positivos', 'Negativos'])
-plt.title('Matriz de Confusão')
-plt.xlabel('Predição')
+            yticklabels=['True', 'False'], 
+            xticklabels=['Positive', 'Negative'])
+plt.title('Confusion Matrix')
+plt.xlabel('Prediction')
 plt.ylabel('Real')
 plt.show()
 
