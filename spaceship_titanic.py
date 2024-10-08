ce5b197a6295213385b2505d9f90b5841f29168c
Victor Simas <victorsimasdev@gmail.com>
2024-10-08 10:59:00 -0300

    feat: Treated null data

--- spaceship_titanic.py
+++ spaceship_titanic.py
@@ -6,5 +6,18 @@ from sklearn.metrics import accuracy_score
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
-print(train)
-print(test)
+
+train.fillna({'HomePlanet': train['HomePlanet'].mode()[0]}, inplace=True)
+train.fillna({'CryoSleep': train['CryoSleep'].mode()[0]}, inplace=True)
+train.fillna({'Destination': train['Destination'].mode()[0]}, inplace=True)
+train.fillna({'VIP': train['VIP'].mode()[0]}, inplace=True)
+train.fillna({'Name': 'Desconhecido'}, inplace=True)
+train.fillna({'Cabin': 'Desconhecido'}, inplace=True)
+train.fillna({'RoomService': 0}, inplace=True)
+train.fillna({'FoodCourt': 0}, inplace=True)
+train.fillna({'ShoppingMall': 0}, inplace=True)
+train.fillna({'Spa': 0}, inplace=True)
+train.fillna({'VRDeck': 0}, inplace=True)
+train.fillna({'Age': train['Age'].median()}, inplace=True)
+
+print(train.isnull().sum())
